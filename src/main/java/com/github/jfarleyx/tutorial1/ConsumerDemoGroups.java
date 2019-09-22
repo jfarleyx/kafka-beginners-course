54858a3bc8a6804fce5569c989d7c7182b172635
John Farley <john.farley1@sykes.com>
2019-09-21 19:09:30 -0500

    additional consumer demos

---
+++ src/main/java/com/github/jfarleyx/tutorial1/ConsumerDemoGroups.java
@@ -0,0 +1,44 @@
+package com.github.jfarleyx.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoGroups {
+
+    public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class.getName());
+
+        // create consumer config
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-fifth-application");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // latest: read since last offset
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // subscribe consumer to our topic(s)
+        consumer.subscribe(Collections.singleton("first_topic"));
+
+        // poll for new data
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0+
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Key: " + record.key() + "; Value: " + record.value());
+                logger.info("Partition: " + record.partition() + "; Offset: " + record.offset());
+            }
+        }
+    }
+}
